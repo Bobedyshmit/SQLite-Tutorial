fe4b4ee76d97feba45ac1ec86af8fe25717e0706
Bobedyshmit <Bobedyshmit@users.noreply.github.com>
2016-08-08 17:39:58 -0400

    Update Database.java. personId now unique field. Use catch to ensure no duplicate peronId is entered instead of another method.

--- Database.java
+++ Database.java
@@ -3,24 +3,21 @@ package com.github.bobedyshmit.sqlitetutorial;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Database implements AutoCloseable {
 
     private static final String PEOPLE_TABLE        = "people";
-    private static final String CREATE_PEOPLE_TABLE = "create table if not exists " + PEOPLE_TABLE + " (personId integer, firstname text, lastname text, age integer check (age >= 0))";
+    private static final String CREATE_PEOPLE_TABLE = "create table if not exists " + PEOPLE_TABLE + " (personId integer not null unique, firstname text, lastname text, age integer check (age >= 0))";
     private static final String INSERT_PERSON       = "insert into " + PEOPLE_TABLE + " values (?, ?, ?, ?)";
     private static final String DELETE_PERSON       = "delete from " + PEOPLE_TABLE + " where (? = personId)";
     private static final String SELECT_PERSON       = "select personId from " + PEOPLE_TABLE;
-    
-    private final int PERSON_ID = 1;
-    private final int FIRST_NAME = 2;
-    private final int LAST_NAME = 3;
-    private final int AGE = 4;
-    
-    
+
+    private final int           PERSON_ID           = 1;
+    private final int           FIRST_NAME          = 2;
+    private final int           LAST_NAME           = 3;
+    private final int           AGE                 = 4;
+
     private final Connection    connection;
 
     public Database() throws SQLException {
@@ -36,13 +33,16 @@ public class Database implements AutoCloseable {
 
     public void insertPerson(final Person person) throws SQLException {
         try (final PreparedStatement stmt = connection.prepareStatement(INSERT_PERSON)) {
-            if (!isPersonEntered(person)) {
-                stmt.setInt(PERSON_ID, person.getPersonId());
-                stmt.setString(FIRST_NAME, person.getFirstName());
-                stmt.setString(LAST_NAME, person.getLastName());
-                stmt.setInt(AGE, person.getAge());
-                stmt.execute();
-            }
+
+            stmt.setInt(PERSON_ID, person.getPersonId());
+            stmt.setString(FIRST_NAME, person.getFirstName());
+            stmt.setString(LAST_NAME, person.getLastName());
+            stmt.setInt(AGE, person.getAge());
+            stmt.execute();
+
+        }
+        catch (final SQLException e) {
+            System.out.println("Person Already Exists");
         }
     }
 
@@ -50,19 +50,6 @@ public class Database implements AutoCloseable {
         try (final PreparedStatement stmt = connection.prepareStatement(DELETE_PERSON)) {
             stmt.setInt(1, person.getPersonId());
             stmt.execute();
-        }
-    }
-
-    public boolean isPersonEntered(final Person person) throws SQLException {
-        try (final Statement stmt = connection.createStatement()) {
-            final ResultSet rs = stmt.executeQuery(SELECT_PERSON);
-            while (rs.next()) {
-                if (person.getPersonId() == rs.getInt(PERSON_ID)) {
-                    return true;
-                }
-            }
-            System.out.println("User alreadt exists");
-            return false;
         }
     }
 
